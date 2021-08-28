b315ff44465cfd04752d11f3e55b45237d84005d
Ernesto Andres Rincon Cruz <ernesto.arc@gmail.com>
2021-08-28 12:14:38 -0500

    Ejemplo de uso libreria LEDs

--- K32L2B31A_Project/source/K32L2B31A_Project.c
+++ K32L2B31A_Project/source/K32L2B31A_Project.c
@@ -18,6 +18,8 @@
 #include "clock_config.h"
 #include "K32L2B31A.h"
 #include "fsl_debug_console.h"
+
+#include "leds.h"
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
@@ -38,6 +40,17 @@ float dato_float=3.1416;
 /*******************************************************************************
  * Private Source Code
  ******************************************************************************/
+/*!
+ * @brief genera bloqueo de microcontrolador por tiempo fijo
+ *
+ */
+void delay_block(void){
+	uint32_t i;
+	for(i=0;i<0xFFFFF;i++){
+
+	}
+}
+
 
 int main(void) {
 	int i = 0 ;/* Force the counter to be placed into memory. */
@@ -55,10 +68,16 @@ int main(void) {
     printf("test_global_var:%d\r\n",test_global_var);
     printf("dato_float:%g\r\n",dato_float);
 
+
+
     /* Enter an infinite loop, just incrementing a counter. */
     while(1) {
         i++ ;
         printf("i:%u\r\n",i);
+        encender_led_verde();
+        delay_block();
+        apagar_led_verde();
+        delay_block();
     }
     return 0 ;
 }
